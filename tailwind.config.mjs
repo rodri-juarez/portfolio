b077bcbfcd97be733c5de732cf949ca4a6c9cd27
Rodrigo Juarez <juarezr100@gmail.com>
2023-11-15 16:49:29 -0300

    refactor: update wording and imgs

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -7,7 +7,12 @@ export default {
     './src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
     './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}'
   ],
-	theme: {
+  theme: {
+    colors: {
+      // Using modern `rgb`
+      primary: 'rgb(var(--color-primary) / <alpha-value>)',
+      secondary: 'rgb(var(--color-secondary) / <alpha-value>)',
+    },
 		extend: {},
 	},
   darkMode: "class",
